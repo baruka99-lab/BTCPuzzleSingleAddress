8e9f505ea3d867a7d957c9ba7d4be99256109cdf
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-20 12:24:28 +0500

    Update BTCPuzz.py

--- Linux/BTCPuzz.py
+++ Linux/BTCPuzz.py
@@ -1,89 +1,91 @@
-from fastecdsa import keys, curve
-from ellipticcurve.privateKey import PrivateKey
-import multiprocessing
+print("Start! Good Luck!")
+
+import ecdsa
+from Crypto.Hash import RIPEMD160
 import hashlib
-import binascii
-import random
+import base58
+from multiprocessing import Pool, cpu_count
+import os
+from fastecdsa import keys, curve
 
 def generate_private_key():
-    return format(random.randint(0, 0x1FFFFFF) & 0x01FFFFFF, '06x').upper().zfill(6)
+    return binascii.hexlify(os.urandom(32)).decode('utf-8').upper()
 
-def private_key_to_public_key(private_key, compressed=True):
-    if compressed:
-        key = keys.get_public_key(int(private_key, 16), curve.secp256k1)
-        return '02' + format(key.x, '064x').upper() if key.y % 2 == 0 else '03' + format(key.x, '064x').upper()
-    else:
-        key = keys.get_public_key(int(private_key, 16), curve.secp256k1)
-        return '04' + (format(key.x, '064x').upper() + format(key.y, '064x').upper())
+def generate_fastecdsa_private_key():
+    return hex(keys.gen_private_key(curve.secp256k1))
 
-def public_key_to_address(public_key):
-    output = []
-    alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-    var = hashlib.new('ripemd160')
-    encoding = binascii.unhexlify(public_key.encode())
-    var.update(hashlib.sha256(encoding).digest())
-    var_encoded = ('00' + var.hexdigest()).encode()
-    digest = hashlib.sha256(binascii.unhexlify(var_encoded)).digest()
-    var_hex = '00' + var.hexdigest() + hashlib.sha256(digest).hexdigest()[0:8]
-    count = [char != '0' for char in var_hex].index(True) // 2
-    n = int(var_hex, 16)
-    while n > 0:
-        n, remainder = divmod(n, 58)
-        output.append(alphabet[remainder])
-    for i in range(count): output.append(alphabet[0])
-    return ''.join(output[::-1])
+def generate_key_pair(args):
+    process_id, start_range, end_range, use_fastecdsa = args
+    random_generator = os.urandom
 
-def private_key_to_wif(private_key):
-    digest = hashlib.sha256(binascii.unhexlify('80' + private_key)).hexdigest()
-    var = hashlib.sha256(binascii.unhexlify(digest)).hexdigest()
-    var = binascii.unhexlify('80' + private_key + var[0:8])
-    alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-    value = pad = 0
-    result = ''
-    for i, c in enumerate(var[::-1]): value += 256**i * c
-    while value >= len(alphabet):
-        div, mod = divmod(value, len(alphabet))
-        result, value = alphabet[mod] + result, div
-    result = alphabet[value] + result
-    for c in var:
-        if c == 0: pad += 1
-        else: break
-    return alphabet[0] * pad + result
+    while True:
+        # Генерация случайного закрытого ключа
+        if use_fastecdsa:
+            private_key = generate_fastecdsa_private_key()
+        else:
+            private_key = generate_private_key()
 
-def check_and_write_address(bitcoin_address, private_key, process_id):
+        # Преобразование закрытого ключа в объект ecdsa.SigningKey
+        private_key = int(private_key, 16)
+        private_key = ecdsa.SigningKey.from_secret_exponent(private_key, curve=ecdsa.SECP256k1)
+
+        # Получение сжатого открытого ключа
+        compressed_public_key = private_key.get_verifying_key().to_string("compressed")
+
+        # Хэширование открытого ключа для получения отпечатка с использованием RIPEMD-160
+        ripemd160_hash = RIPEMD160.new(hashlib.sha256(compressed_public_key).digest()).digest()
+
+        # Добавление префикса к хэшу (для Bitcoin-адреса)
+        prefixed_public_key_hash = b'\x00' + ripemd160_hash  # 0x00 для основной сети
+
+        # Вычисление контрольной суммы
+        checksum = hashlib.sha256(hashlib.sha256(prefixed_public_key_hash).digest()).digest()[:4]
+
+        # Формирование Bitcoin-адреса в кодировке base58
+        bitcoin_address = base58.b58encode(prefixed_public_key_hash + checksum).decode('utf-8')
+
+        #print(f"Process {process_id}: Private Key: {private_key.to_string().hex()}")
+        #print(f"Process {process_id}: Compressed Public Key: {compressed_public_key.hex()}")
+        #print(f"Process {process_id}: Bitcoin Address: {bitcoin_address}\n")
+
+        # Проверка и запись в файл found.txt или address.txt
+        if check_and_write_address(bitcoin_address, private_key, compressed_public_key, process_id):
+            # Прерывание цикла, если найден целевой адрес
+            break
+
+def check_and_write_address(bitcoin_address, private_key, compressed_public_key, process_id):
     # Проверка на наличие конкретного адреса
     target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"  # Целевой адрес
     if bitcoin_address == target_address:
+        # Запись найденного адреса в файл
+        with open('found.txt', 'a') as found_file:
+            found_file.write(f"Найден целевой адрес: {bitcoin_address}\n")
+            found_file.write(f"Закрытый ключ: {private_key.to_string().hex()}\n")
         print("Целевой адрес найден!")
-        print(f"Процесс {process_id}: Закрытый ключ: {private_key}")
+        print(f"Процесс {process_id}: Закрытый ключ: {private_key.to_string().hex()}")
+        print(f"Процесс {process_id}: Сжатый открытый ключ: {compressed_public_key.hex()}")
         print(f"Процесс {process_id}: Bitcoin-адрес: {bitcoin_address}\n")
         return True
 
     return False
 
-def generate_key_pair(process_id):
-    while True:
-        private_key = generate_private_key()
-        public_key = private_key_to_public_key(private_key, compressed=True) 
-        address = public_key_to_address(public_key)
-
-        if address[-4:] == "TEST":  # Проверка на "TEST" для ускорения процесса
-            continue
-
-        print(f"Процесс {process_id}: Закрытый ключ: {private_key}")
-        print(f"Процесс {process_id}: Сжатый Bitcoin-адрес: {address}\n")
-
-        if check_and_write_address(address, private_key, process_id):
-            break
-
 if __name__ == '__main__':
-    num_processes = multiprocessing.cpu_count()
-    process_list = []
+    args = {
+        'verbose': 0,
+        'substring': 8,
+        'fastecdsa': True,  # Установите в True для использования fastecdsa
+        'cpu_count': multiprocessing.cpu_count(),
+    }
+    
+    num_processes = args['cpu_count']
+    pool = Pool(num_processes)
 
-    for i in range(num_processes):
-        process = multiprocessing.Process(target=generate_key_pair, args=(i,))
-        process_list.append(process)
-        process.start()
+    # Указанный диапазон для генерации secret_exponent
+    start_range = int("0000000000000000000000000000000000000000000000020000000000000000", 16)
+    end_range = int("000000000000000000000000000000000000000000000003ffffffffffffffff", 16)
 
-    for process in process_list:
-        process.join()
+    # Запуск каждого процесса с уникальным идентификатором
+    pool.map(generate_key_pair, [(i, start_range, end_range, args['fastecdsa']) for i in range(num_processes)])
+
+    pool.close()
+    pool.join()
