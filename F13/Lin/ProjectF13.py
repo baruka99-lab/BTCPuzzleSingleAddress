ac4bce45e8d3dc36f6b17ce1b2198cb77e2d374e
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 11:36:04 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -1,7 +1,7 @@
 print("Start Project13")
 
 import ecdsa
-from Crypto.Hash import hashlib_new as hashlib
+from Crypto.Hash import new as hashlib
 import base58
 import secrets
 from concurrent.futures import ProcessPoolExecutor
@@ -13,10 +13,10 @@ def generate_key_pair(private_key):
     base_private_key_point = base_point * private_key
 
     base_public_key_bytes = ecdsa.VerifyingKey.from_public_point(base_private_key_point, curve).to_string("compressed")
-    sha256_hash = hashlib.new("sha256", base_public_key_bytes).digest()
-    ripemd160_hash = hashlib.new("ripemd160", sha256_hash).digest()
+    sha256_hash = hashlib("sha256", base_public_key_bytes).digest()
+    ripemd160_hash = hashlib("ripemd160", sha256_hash).digest()
     network_byte = b"\x00"
-    checksum = hashlib.new("sha256", hashlib.new("sha256", network_byte + ripemd160_hash).digest()).digest()[:4]
+    checksum = hashlib("sha256", hashlib("sha256", network_byte + ripemd160_hash).digest()).digest()[:4]
     address = base58.b58encode(network_byte + ripemd160_hash + checksum).decode("utf-8")
 
     return private_key, address
