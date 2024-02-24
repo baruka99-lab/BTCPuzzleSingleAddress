875dbd0c51bd26253ba961ec46ad899d2943728c
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 09:52:19 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -2,13 +2,13 @@ import hashlib
 import base58
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from multiprocessing import cpu_count
-from fastecdsa import keys, curve
+from fastecdsa import keys, curve, point
 
 def generate_key_pair(private_key):
     base_point = curve.secp256k1.G
-    base_private_key_point = base_point * private_key
+    base_private_key_point = point.Multiply(curve.secp256k1.G, private_key)
 
-    base_public_key_bytes = keys.get_public_key(base_private_key_point, curve=curve.secp256k1, compressed=True)
+    base_public_key_bytes = keys.get_public_key(base_private_key_point, curve=curve.secp256k1)
     sha256_hash = hashlib.sha256(base_public_key_bytes).digest()
     ripemd160_hash = hashlib.new("ripemd160", sha256_hash).digest()
     network_byte = b"\x00"
