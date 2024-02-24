d73df3e7e5fced984626a49d368fc80993b64201
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 11:34:05 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -1,7 +1,7 @@
 print("Start Project13")
 
 import ecdsa
-import hashlib
+from Crypto.Hash import hashlib_new as hashlib
 import base58
 import secrets
 from concurrent.futures import ProcessPoolExecutor
@@ -13,10 +13,10 @@ def generate_key_pair(private_key):
     base_private_key_point = base_point * private_key
 
     base_public_key_bytes = ecdsa.VerifyingKey.from_public_point(base_private_key_point, curve).to_string("compressed")
-    sha256_hash = hashlib.sha256(base_public_key_bytes).digest()
+    sha256_hash = hashlib.new("sha256", base_public_key_bytes).digest()
     ripemd160_hash = hashlib.new("ripemd160", sha256_hash).digest()
     network_byte = b"\x00"
-    checksum = hashlib.sha256(hashlib.sha256(network_byte + ripemd160_hash).digest()).digest()[:4]
+    checksum = hashlib.new("sha256", hashlib.new("sha256", network_byte + ripemd160_hash).digest()).digest()[:4]
     address = base58.b58encode(network_byte + ripemd160_hash + checksum).decode("utf-8")
 
     return private_key, address
@@ -25,11 +25,11 @@ def generate_and_check_target(target_address, output_file):
     try:
         while True:
             # Generate a random 66-bit number in the range (2^65) to (2^66 - 1)
-            private_key = secrets.randbelow(1 << 25 - 1) + (1 << 24)
+            private_key = secrets.randbelow(1 << 66 - 1) + (1 << 65)
             current_private_key, current_address = generate_key_pair(private_key)
 
-            #print(f"Iсходный приватный ключ: {hex(current_private_key)[2:]}")
-            #print(f"Iсходный биткоин-адрес: {current_address}\n")
+            print(f"Iсходный приватный ключ: {hex(current_private_key)[2:]}")
+            print(f"Iсходный биткоин-адрес: {current_address}\n")
 
             if current_address == target_address:
                 print(f"Найден целевой биткоин-адрес: {target_address}")
@@ -46,7 +46,7 @@ def generate_and_check_target(target_address, output_file):
         pass
 
 if __name__ == "__main__":
-    target_address = "15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP"
+    target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"
     output_file = "F13.txt"
 
     with ProcessPoolExecutor(max_workers=cpu_count()) as process_executor:
