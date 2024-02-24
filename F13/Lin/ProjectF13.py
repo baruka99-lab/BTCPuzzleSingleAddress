a19c46837f903ce86a784387ffc3a71095a42247
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 11:28:48 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -25,15 +25,15 @@ def generate_and_check_target(target_address, output_file):
     try:
         while True:
             # Generate a random 66-bit number in the range (2^65) to (2^66 - 1)
-            private_key = secrets.randbelow(1 << 66 - 1) + (1 << 65)
+            private_key = secrets.randbelow(1 << 25 - 1) + (1 << 24)
             current_private_key, current_address = generate_key_pair(private_key)
 
             print(f"Iсходный приватный ключ: {hex(current_private_key)[2:]}")
             print(f"Iсходный биткоин-адрес: {current_address}\n")
 
             if current_address == target_address:
-                print(f"Найден целевой биткоин-адрес: {target_address}")
-                print(f"Приватный ключ для целевого адреса: {hex(current_private_key)[2:]}")
+                #print(f"Найден целевой биткоин-адрес: {target_address}")
+                #print(f"Приватный ключ для целевого адреса: {hex(current_private_key)[2:]}")
 
                 # Запись в файл
                 with open(output_file, "a") as file:
@@ -46,7 +46,7 @@ def generate_and_check_target(target_address, output_file):
         pass
 
 if __name__ == "__main__":
-    target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"
+    target_address = "15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP"
     output_file = "F13.txt"
 
     with ProcessPoolExecutor(max_workers=cpu_count()) as process_executor:
