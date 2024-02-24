4e74ba2411794f29be469ea63551a04b74f4c481
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 14:36:27 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -1,5 +1,3 @@
-print("Start!")
-
 from Crypto.Random import get_random_bytes
 from ecdsa import SigningKey, SECP256k1
 import hashlib
@@ -14,14 +12,15 @@ def generate_custom_address_parallel(args):
         private_key_bytes = get_random_bytes(9)
         private_key = int.from_bytes(private_key_bytes, 'big')
 
-        if private_key.bit_length() > 25:
-            private_key >>= (private_key.bit_length() - 25)
+        if private_key.bit_length() > 66:
+            private_key >>= (private_key.bit_length() - 66)
 
         private_key_obj = SigningKey.from_secret_exponent(private_key, curve=SECP256k1)
         public_key = private_key_obj.get_verifying_key().to_string()
         address_compressed = hashlib.new('ripemd160', hashlib.sha256(public_key).digest()).hexdigest()
 
-        #print("Процесс {}: Итерация {}: Генерация приватного ключа: {}".format(process_id, count, private_key_obj.to_string().hex()))
+        print("Процесс {}: Итерация {}: Генерация приватного ключа: {}".format(process_id, count, private_key_obj.to_string().hex()))
+        print("Процесс {}: Адрес: {}".format(process_id, address_compressed))
 
         if address_compressed == target_address:
             print("\nПроцесс {}: Совпадение найдено после {} итераций.".format(process_id, count))
@@ -37,7 +36,7 @@ def generate_custom_address_parallel(args):
             return address_compressed, private_key_obj.to_string().hex()
 
 if __name__ == "__main__":
-    target_address = "15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP"
+    target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"
     processes = cpu_count()  # Получаем количество ядер CPU
     pool = Pool(processes=processes)
     args_list = [(target_address, i) for i in range(processes)]
