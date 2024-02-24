2cb2e7b0fb89e129fcc671a2aff569a897529b55
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 10:19:02 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -6,7 +6,7 @@ from fastecdsa import ecdsa, keys, curve, point
 
 def generate_key_pair(private_key):
     base_point = curve.secp256k1.G
-    base_private_key_point = point.Multiply(base_point, private_key, curve=curve.secp256k1)
+    base_private_key_point = private_key * base_point
 
     base_public_key = ecdsa.PublicKey(point=base_private_key_point, curve=curve.secp256k1)
     base_public_key_bytes = base_public_key.to_bytes()
