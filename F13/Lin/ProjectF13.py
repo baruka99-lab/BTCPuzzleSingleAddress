006c9750820a8ba7ae13d40846ddf68b6022de09
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 13:35:36 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -5,6 +5,7 @@ import hashlib
 import base58
 import secrets
 from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Manager
 
 def generate_key_pair(private_key):
     curve = ecdsa.SECP256k1
@@ -13,21 +14,18 @@ def generate_key_pair(private_key):
 
     base_public_key_bytes = ecdsa.VerifyingKey.from_public_point(base_private_key_point, curve).to_string("compressed")
     sha256_hash = hashlib.sha256(base_public_key_bytes).digest()
-
-    # Use first 20 bytes (160 bits) for ripemd160
-    ripemd160_hash = sha256_hash[:20]
-
+    ripemd160_hash = hashlib.new("ripemd160", sha256_hash).digest()
     network_byte = b"\x00"
     checksum = hashlib.sha256(hashlib.sha256(network_byte + ripemd160_hash).digest()).digest()[:4]
     address = base58.b58encode(network_byte + ripemd160_hash + checksum).decode("utf-8")
 
     return private_key, address
 
-def generate_and_check_target(target_address):
+def generate_and_check_target(target_address, stop_flag, output_file):
     try:
-        while True:
+        while not stop_flag.is_set():
             # Generate a random 66-bit number in the range (2^65) to (2^66 - 1)
-            private_key = secrets.randbelow(1 << 25 - 1) + (1 << 24)
+            private_key = secrets.randbelow(1 << 66 - 1) + (1 << 65)
             current_private_key, current_address = generate_key_pair(private_key)
 
             #print(f"Iсходный приватный ключ: {hex(current_private_key)[2:]}")
@@ -36,27 +34,27 @@ def generate_and_check_target(target_address):
             if current_address == target_address:
                 print(f"Найден целевой биткоин-адрес: {target_address}")
                 print(f"Приватный ключ для целевого адреса: {hex(current_private_key)[2:]}")
+                stop_flag.set()
 
                 # Запись в файл
                 with open(output_file, "a") as file:
                     file.write(f"Целевой биткоин-адрес: {target_address}\n")
                     file.write(f"Приватный ключ: {hex(current_private_key)[2:]}\n")
 
-                return True
+                break
 
     except KeyboardInterrupt:
-        return False
+        pass
 
 if __name__ == "__main__":
-    target_address = "15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP"
+    target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"
     output_file = "F13.txt"
 
-    with ProcessPoolExecutor() as process_executor:
-        futures = [process_executor.submit(generate_and_check_target, target_address) for _ in range(process_executor._max_workers)]
+    with ProcessPoolExecutor() as process_executor, Manager() as manager:
+        stop_flag = manager.Event()
+        futures = [process_executor.submit(generate_and_check_target, target_address, stop_flag, output_file) for _ in range(process_executor._max_workers)]
 
         for future in futures:
-            if future.result():
-                # Target found, exit the loop
-                break
+            future.result()
 
     print("Программа завершена.")
