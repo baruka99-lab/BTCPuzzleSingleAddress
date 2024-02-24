5c735678fa86a9c54b45c18b645e2f376127a11b
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 14:38:27 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -2,6 +2,7 @@ from Crypto.Random import get_random_bytes
 from ecdsa import SigningKey, SECP256k1
 import hashlib
 from multiprocessing import Pool, cpu_count
+import base58
 
 def generate_custom_address_parallel(args):
     target_address, process_id = args
@@ -17,23 +18,32 @@ def generate_custom_address_parallel(args):
 
         private_key_obj = SigningKey.from_secret_exponent(private_key, curve=SECP256k1)
         public_key = private_key_obj.get_verifying_key().to_string()
-        address_compressed = hashlib.new('ripemd160', hashlib.sha256(public_key).digest()).hexdigest()
+        address_compressed = hashlib.new('ripemd160', hashlib.sha256(public_key).digest()).digest()
+
+        # Добавляем префикс версии биткоин-адреса (для основной сети)
+        address_with_prefix = b"\x00" + address_compressed
+
+        # Вычисляем контрольную сумму
+        checksum = hashlib.sha256(hashlib.sha256(address_with_prefix).digest()).digest()[:4]
+
+        # Собираем полный биткоин-адрес
+        bitcoin_address = base58.b58encode(address_with_prefix + checksum).decode('utf-8')
 
         print("Процесс {}: Итерация {}: Генерация приватного ключа: {}".format(process_id, count, private_key_obj.to_string().hex()))
-        print("Процесс {}: Адрес: {}".format(process_id, address_compressed))
+        print("Процесс {}: Биткоин-адрес: {}".format(process_id, bitcoin_address))
 
-        if address_compressed == target_address:
+        if bitcoin_address == target_address:
             print("\nПроцесс {}: Совпадение найдено после {} итераций.".format(process_id, count))
-            print("Пользовательский Bitcoin-адрес:", address_compressed)
+            print("Пользовательский Биткоин-адрес:", bitcoin_address)
             print("Приватный ключ:", private_key_obj.to_string().hex())
 
             # Запись в файл found13.txt
             with open('found13.txt', 'w') as file:
-                file.write("Bitcoin Address: {}\n".format(address_compressed))
+                file.write("Bitcoin Address: {}\n".format(bitcoin_address))
                 file.write("Private Key: {}\n".format(private_key_obj.to_string().hex()))
 
             print("Информация записана в файл found13.txt.")
-            return address_compressed, private_key_obj.to_string().hex()
+            return bitcoin_address, private_key_obj.to_string().hex()
 
 if __name__ == "__main__":
     target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"
