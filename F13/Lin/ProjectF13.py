4cb4aaede4cad2bdeff2f1d18a731a72b412bd4d
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-02-24 14:16:45 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -23,20 +23,21 @@ def generate_key_pair(private_key):
 def generate_and_check_target(args):
     target_address, start, end = args
     for _ in range(start, end):
-        current_private_key = secrets.randbelow(ecdsa.SECP256k1.order)
+        # Генерация случайного числа с битовой длиной 25
+        current_private_key = secrets.randbits(25)
         current_private_key, current_address = generate_key_pair(current_private_key)
 
         if current_address == target_address:
             print(f"Найден целевой биткоин-адрес: {target_address}")
-            print(f"Приватный ключ для целевого адреса: {hex(current_private_key)[2:]}")
+            print(f"Приватный ключ для целевого адреса: {bin(current_private_key)[2:]}")
 
             with open("F13.txt", "a") as file:
                 file.write(f"Целевой биткоин-адрес: {target_address}\n")
-                file.write(f"Приватный ключ: {hex(current_private_key)[2:]}\n")
+                file.write(f"Приватный ключ: {bin(current_private_key)[2:]}\n")
 
             return True
 
-        print(f"Private Key: {hex(current_private_key)[2:]} | Address: {current_address}")
+        print(f"Private Key: {bin(current_private_key)[2:]} | Address: {current_address}")
 
     return False
 
@@ -47,8 +48,8 @@ if __name__ == "__main__":
 
     with ProcessPoolExecutor(max_workers=num_processes) as executor:
         chunk_size = 2**24
-        start_values = [(1 << 24)]  # Изменено начальное значение
-        end_values = [(1 << 25 - 1) + (1 << 24)]  # Изменено конечное значение
+        start_values = [0]
+        end_values = [1 << 25]
         args_list = [(target_address, start, end) for start, end in zip(start_values, end_values)]
 
         try:
