9579a405c8b99b8eaf457589f9e6035918ddb984
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-03-20 01:19:35 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -1,3 +1,5 @@
+print("Start!")
+
 from fastecdsa import keys, curve
 from multiprocessing import cpu_count, Pool
 import hashlib
@@ -8,7 +10,7 @@ def generate_private_key():
     return hex((random.randrange((1 << 25) - 1) + (1 << 24)))[2:].upper().zfill(64)
 
 def private_key_to_public_key(private_key, compressed=True):
-    key = keys.get_public_key(int(private_key), curve.secp256k1)
+    key = keys.get_public_key(int(private_key, 16), curve.secp256k1)
     if compressed:
         return '02' + hex(key.x)[2:].zfill(64) if key.y % 2 == 0 else '03' + hex(key.x)[2:].zfill(64)
     else:
@@ -17,7 +19,7 @@ def private_key_to_public_key(private_key, compressed=True):
 def public_key_to_address(public_key):
     alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
     var = hashlib.new('ripemd160')
-    encoding = binascii.unhexlify(public_key)
+    encoding = binascii.unhexlify(public_key.encode())
     var.update(hashlib.sha256(encoding).digest())
     var_encoded = ('00' + var.hexdigest()).encode()
     digest = hashlib.sha256(binascii.unhexlify(var_encoded)).digest()
@@ -32,30 +34,23 @@ def public_key_to_address(public_key):
         output.append(alphabet[0])
     return ''.join(output[::-1])
 
-def generate_key_pair(process_id, target_address, compressed=True):
+def generate_key_pair(process_id, target_address):
     while True:
         private_key = generate_private_key()
-        public_key = private_key_to_public_key(private_key, compressed=compressed)
+        public_key = private_key_to_public_key(private_key)
         address = public_key_to_address(public_key)
+        
+        print(f"Process {process_id}: Private Key: {private_key}")
+        print(f"Process {process_id}: Bitcoin Address: {address}\n")
 
         # Check and write address to file
-        if check_and_write_address(process_id, public_key, address, private_key, target_address):
+        if address == target_address:
+            print(f"Process {process_id}: Target Address Found!")
+            with open('F13.txt', 'a') as found_file:
+                found_file.write(f"Found Target Address: {address}\n")
+                found_file.write(f"Private Key (Hex): {private_key}\n")
+                found_file.write(f"Public Key: {public_key}\n")
             break
-
-def check_and_write_address(process_id, public_key, bitcoin_address, private_key, target_address):
-    print(f"Process {process_id}: Private Key: {private_key}")
-    print(f"Process {process_id}: Bitcoin Address: {bitcoin_address}\n")
-
-    if bitcoin_address == target_address:
-        print(f"Process {process_id}: Target Address Found!")
-        print(f"Target Address: {bitcoin_address}")
-        print(f"Private Key: {private_key}")
-        with open('F13.txt', 'a') as found_file:
-            found_file.write(f"Found Target Address: {bitcoin_address}\n")
-            found_file.write(f"Private Key (Hex): {private_key}\n")
-            found_file.write(f"Public Key: {public_key}\n")
-        return True
-    return False
 
 if __name__ == '__main__':
     num_processes = cpu_count()
@@ -63,7 +58,7 @@ if __name__ == '__main__':
     target_address = "15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP"  # Целевой адрес
 
     # Start each process with a unique identifier
-    pool.starmap(generate_key_pair, [(i, target_address, True) for i in range(num_processes)])
+    pool.starmap(generate_key_pair, [(i, target_address) for i in range(num_processes)])
 
     pool.close()
     pool.join()
