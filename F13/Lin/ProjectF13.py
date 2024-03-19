53a4ed1b085214a86c01c6ca499edbfc8ed9d416
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-03-20 00:29:01 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -9,19 +9,30 @@ import random
 def generate_private_key():
     return hex((random.randrange(1 << 25 - 1) + (1 << 24)))[2:].upper().zfill(64)
 
-def private_key_to_public_key(private_key, fastecdsa=True):
+def private_key_to_public_key(private_key, fastecdsa=True, compressed=True):
     if fastecdsa:
         key = keys.get_public_key(int('0x' + private_key, 0), curve.secp256k1)
-        return '04' + (hex(key.x)[2:] + hex(key.y)[2:]).zfill(128)
+        x_hex = hex(key.x)[2:].zfill(64)
+        y_hex = hex(key.y)[2:].zfill(64)
+        if compressed:
+            prefix = '02' if key.y % 2 == 0 else '03'
+            return prefix + x_hex
+        else:
+            return '04' + x_hex + y_hex
     else:
         pk = PrivateKey().fromString(bytes.fromhex(private_key))
-        return '04' + pk.publicKey().toString().hex().upper()
+        if compressed:
+            return '02' + pk.publicKey().toString().hex().upper()[:64]
+        else:
+            return '04' + pk.publicKey().toString().hex().upper()
 
-def public_key_to_address(public_key):
+def public_key_to_address(public_key, compressed=True):
     output = []
     alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
     var = hashlib.new('ripemd160')
     encoding = binascii.unhexlify(public_key.encode())
+    if compressed:
+        encoding = binascii.unhexlify(compress_public_key(public_key))
     var.update(hashlib.sha256(encoding).digest())
     var_encoded = ('00' + var.hexdigest()).encode()
     digest = hashlib.sha256(binascii.unhexlify(var_encoded)).digest()
@@ -33,6 +44,12 @@ def public_key_to_address(public_key):
         output.append(alphabet[remainder])
     for i in range(count): output.append(alphabet[0])
     return ''.join(output[::-1])
+
+def compress_public_key(public_key):
+    # Compress public key by removing the prefix and using only x-coordinate
+    prefix = public_key[:2]
+    x_coordinate = public_key[2:]
+    return x_coordinate
 
 def private_key_to_wif(private_key):
     digest = hashlib.sha256(binascii.unhexlify('80' + private_key)).hexdigest()
@@ -51,11 +68,11 @@ def private_key_to_wif(private_key):
         else: break
     return chars[0] * pad + result
 
-def generate_key_pair(process_id, target_address):
+def generate_key_pair(process_id, target_address, compressed=True):
     while True:
         private_key = generate_private_key()
-        public_key = private_key_to_public_key(private_key)
-        address = public_key_to_address(public_key)
+        public_key = private_key_to_public_key(private_key, compressed=compressed)
+        address = public_key_to_address(public_key, compressed=compressed)
         wif = private_key_to_wif(private_key)
 
         # Check and write address to file
@@ -78,10 +95,10 @@ def check_and_write_address(process_id, public_key, bitcoin_address, private_key
 if __name__ == '__main__':
     num_processes = cpu_count()
     pool = Pool(num_processes)
-    target_address = "1Pner9KEtCMgsgU1nUE8vwbEY7nbcgN7F2"  # Целевой адрес
+    target_address = "15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP"  # Целевой адрес
 
     # Start each process with a unique identifier
-    pool.starmap(generate_key_pair, [(i, target_address) for i in range(num_processes)])
+    pool.starmap(generate_key_pair, [(i, target_address, True) for i in range(num_processes)])
 
     pool.close()
     pool.join()
