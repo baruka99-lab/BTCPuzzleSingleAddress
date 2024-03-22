dafaaef6e7b452d749cc060dac2dcca3c2b327af
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-03-22 10:00:22 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -7,7 +7,7 @@ import binascii
 import random
 
 def generate_private_key():
-    return hex((random.randrange((1 << 159) - 1) + (1 << 159)))[2:].upper().zfill(64)
+    return hex((random.randrange((1 << 256) - 1) - (1 << 190)))[2:].upper().zfill(64)
 
 def private_key_to_public_key(private_key, compressed=True):
     key = keys.get_public_key(int(private_key, 16), curve.secp256k1)
