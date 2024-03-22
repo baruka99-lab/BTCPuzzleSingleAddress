4feb1741478c5206b659b159135886997f5a09a4
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-03-22 17:11:25 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -1,21 +1,67 @@
-import secp256k1 as ice
+print("Start!")
 
-def generate_and_check_private_key(start_range, end_range, target_address, output_file):
-    for i in range(start_range, end_range):
-        try:
-            address = ice.privatekey_to_address(0, True, i)
-            if address == target_address:
-                print(f"Private Key: {i}")
-                print(f"Address: {address}")
-                with open(output_file, "a") as file:
-                    file.write(f"Private Key: {i}\n")
-                    file.write(f"Address: {address}\n\n")
-        except Exception as e:
-            print(f"Error occurred for private key {i}: {e}")
+from fastecdsa import keys, curve
+from multiprocessing import cpu_count, Pool
+import hashlib
+import binascii
+import random
 
-if __name__ == "__main__":
-    start_range = 2**65
-    end_range = (2**66) - 1
-    target_address = '13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so'
-    output_file = "F13.txt"
-    generate_and_check_private_key(start_range, end_range, target_address, output_file)
+def generate_private_key():
+    while True:
+        private_key = hex((random.randrange((1 << 65) - 1) + (1 << 65)))[2:].zfill(64)
+        public_key = private_key_to_public_key(private_key)
+        address = public_key_to_address(public_key)
+        if address.startswith('13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so'):  # Проверяем, начинается ли адрес на "13z"
+            return private_key
+
+def private_key_to_public_key(private_key, compressed=True):
+    key = keys.get_public_key(int(private_key, 16), curve.secp256k1)
+    if compressed:
+        return '02' + hex(key.x)[2:].zfill(64) if key.y % 2 == 0 else '03' + hex(key.x)[2:].zfill(64)
+    else:
+        return '04' + (hex(key.x)[2:].zfill(64) + hex(key.y)[2:].zfill(64))
+
+def public_key_to_address(public_key):
+    alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    var = hashlib.new('ripemd160')
+    encoding = binascii.unhexlify(public_key.encode())
+    var.update(hashlib.sha256(encoding).digest())
+    var_encoded = ('00' + var.hexdigest()).encode()
+    digest = hashlib.sha256(binascii.unhexlify(var_encoded)).digest()
+    var_hex = '00' + var.hexdigest() + hashlib.sha256(digest).hexdigest()[0:8]
+    count = [char != '0' for char in var_hex].index(True) // 2
+    n = int(var_hex, 16)
+    output = []
+    while n > 0:
+        n, remainder = divmod(n, 58)
+        output.append(alphabet[remainder])
+    for i in range(count):
+        output.append(alphabet[0])
+    return ''.join(output[::-1])
+
+def generate_key_pair(process_id, compressed=True):
+    while True:
+        private_key = generate_private_key()
+        public_key = private_key_to_public_key(private_key, compressed=compressed)
+        address = public_key_to_address(public_key)
+        write_and_print_results(public_key, address, private_key)
+
+def write_and_print_results(public_key, address, private_key):
+    with open('F13.txt', 'a') as found_file:
+        found_file.write(f"Found Address: {address}\n")
+        found_file.write(f"Private Key (Hex): {private_key}\n")
+        found_file.write(f"Public Key: {public_key}\n")
+    print(f"Found Address: {address}")
+    print(f"Private Key (Hex): {private_key}")
+    print(f"Public Key: {public_key}")
+    print()
+
+if __name__ == '__main__':
+    num_processes = cpu_count()
+    pool = Pool(num_processes)
+
+    # Start each process with a unique identifier
+    pool.starmap(generate_key_pair, [(i,) for i in range(num_processes)])
+
+    pool.close()
+    pool.join()
