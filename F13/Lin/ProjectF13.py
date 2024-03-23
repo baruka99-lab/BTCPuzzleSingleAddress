a171f9d2fa28aeada030969b4e9b038a12a0dd16
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-03-23 14:03:27 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -5,8 +5,8 @@ import binascii
 import random
 
 def generate_private_key_decimal():
-    min_value = 1 << 65
-    max_value = (1 << 66) - 1
+    min_value = 1 << 159
+    max_value = (1 << 160) - 1
     return str(random.randint(min_value, max_value))  # Генерация случайного числа в заданном диапазоне
 
 def private_key_to_public_key(private_key, compressed=True):
@@ -45,8 +45,8 @@ def generate_key_pair(process_id, target_address, compressed=True):
             break
 
 def check_and_write_address(process_id, public_key, bitcoin_address, private_key, target_address):
-    print(f"Process {process_id}: Private Key: {private_key}")
-    print(f"Process {process_id}: Bitcoin Address: {bitcoin_address}\n")
+    #print(f"Process {process_id}: Private Key: {private_key}")
+    #print(f"Process {process_id}: Bitcoin Address: {bitcoin_address}\n")
 
     if bitcoin_address == target_address:
         print(f"Process {process_id}: Target Address Found!")
@@ -62,7 +62,7 @@ def check_and_write_address(process_id, public_key, bitcoin_address, private_key
 if __name__ == '__main__':
     num_processes = cpu_count()
     pool = Pool(num_processes)
-    target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"  # Целевой адрес
+    target_address = "1NBC8uXJy1GiJ6drkiZa1WuKn51ps7EPTv"  # Целевой адрес
 
     # Start each process with a unique identifier
     pool.starmap(generate_key_pair, [(i, target_address) for i in range(num_processes)])
