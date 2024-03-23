8205a16c6cbfe2d91f55039f324a5db2ad5c8adf
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-03-23 11:38:47 +0500

    Update s256k1.py

--- secp256k1/s256k1.py
+++ secp256k1/s256k1.py
@@ -2,7 +2,7 @@ import secp256k1 as ice
 import hashlib
 import base58
 import secrets
-import bitcoin
+from bitcoinlib.keys import PrivateKey
 
 def generate_bitcoin_private_key():
     # Generate a random 256-bit integer (private key)
@@ -12,7 +12,7 @@ def generate_bitcoin_private_key():
     wif_private_key = private_key_to_wif(private_key_int.to_bytes(32, 'big'))
     
     # Get the compressed Bitcoin address from the private key
-    bitcoin_address = private_key_to_address(private_key_int, compressed=True)
+    bitcoin_address = private_key_to_address(private_key_int)
     
     return wif_private_key, bitcoin_address
 
@@ -35,9 +35,12 @@ def private_key_to_wif(private_key_bytes):
     
     return wif_private_key.decode('utf-8')
 
-def private_key_to_address(private_key_int, compressed=True):
-    # Get the compressed or uncompressed Bitcoin address
-    bitcoin_address = bitcoin.privkey_to_address(private_key_int, compressed=compressed)
+def private_key_to_address(private_key_int):
+    # Create PrivateKey object from integer private key
+    private_key = PrivateKey(private_key_int)
+    
+    # Get the compressed Bitcoin address
+    bitcoin_address = private_key.address(compressed=True)
     
     return bitcoin_address
 
