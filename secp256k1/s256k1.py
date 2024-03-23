7b8bdae16fa398bfa1162c0824f2185a971882bb
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-03-23 11:31:25 +0500

    Update s256k1.py

--- secp256k1/s256k1.py
+++ secp256k1/s256k1.py
@@ -1,19 +1,23 @@
 import secp256k1 as ice
+import secrets
 
 def generate_bitcoin_private_key():
-    # Генерируем случайное число, которое будет приватным ключом
-    private_key = ice.random_scalar()
+    # Generate a random 32-byte number (private key)
+    private_key_bytes = secrets.token_bytes(32)
     
-    # Преобразуем приватный ключ в формат WIF (Wallet Import Format)
+    # Convert the private key bytes to an integer
+    private_key = int.from_bytes(private_key_bytes, byteorder='big')
+    
+    # Convert the private key to WIF format
     wif_private_key = ice.btc_pvk_to_wif(private_key)
     
-    # Получаем адрес кошелька на основе приватного ключа
+    # Get the Bitcoin address from the private key
     bitcoin_address = ice.privatekey_to_coinaddress(ice.COIN_BTC, 0, True, private_key)
     
     return wif_private_key, bitcoin_address
 
-# Пример использования:
-for _ in range(10):  # Генерируем 10 приватных ключей и соответствующих адресов
+# Example usage:
+for _ in range(10):  # Generate 10 random private keys and their corresponding Bitcoin addresses
     private_key, address = generate_bitcoin_private_key()
     print("Private Key:", private_key)
     print("Bitcoin Address:", address)
