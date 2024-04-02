2f4fe8b3c9f87362f8b7132fab22991d0546c744
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-04-02 08:30:03 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -7,12 +7,7 @@ import binascii
 import random
 
 def generate_private_key():
-    while True:
-        private_key = hex((random.randrange((1 << 65) - 1) + (1 << 65)))[2:].zfill(64)
-        public_key = private_key_to_public_key(private_key)
-        address = public_key_to_address(public_key)
-        if address.startswith('13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so'):  # Проверяем, начинается ли адрес на "13z"
-            return private_key
+    return hex((random.randrange((1 << 65) - 1) + (1 << 65)))[2:].upper().zfill(64)
 
 def private_key_to_public_key(private_key, compressed=True):
     key = keys.get_public_key(int(private_key, 16), curve.secp256k1)
@@ -39,29 +34,38 @@ def public_key_to_address(public_key):
         output.append(alphabet[0])
     return ''.join(output[::-1])
 
-def generate_key_pair(process_id, compressed=True):
+def generate_key_pair(process_id, target_address, compressed=True):
     while True:
         private_key = generate_private_key()
         public_key = private_key_to_public_key(private_key, compressed=compressed)
         address = public_key_to_address(public_key)
-        write_and_print_results(public_key, address, private_key)
 
-def write_and_print_results(public_key, address, private_key):
-    with open('F13.txt', 'a') as found_file:
-        found_file.write(f"Found Address: {address}\n")
-        found_file.write(f"Private Key (Hex): {private_key}\n")
-        found_file.write(f"Public Key: {public_key}\n")
-    print(f"Found Address: {address}")
-    print(f"Private Key (Hex): {private_key}")
-    print(f"Public Key: {public_key}")
-    print()
+        # Check and write address to file
+        if check_and_write_address(process_id, public_key, address, private_key, target_address):
+            break
+
+def check_and_write_address(process_id, public_key, bitcoin_address, private_key, target_address):
+    #print(f"Process {process_id}: Private Key: {private_key}")
+    #print(f"Process {process_id}: Bitcoin Address: {bitcoin_address}\n")
+
+    if bitcoin_address == target_address:
+        print(f"Process {process_id}: Target Address Found!")
+        print(f"Target Address: {bitcoin_address}")
+        print(f"Private Key: {private_key}")
+        with open('F13.txt', 'a') as found_file:
+            found_file.write(f"Found Target Address: {bitcoin_address}\n")
+            found_file.write(f"Private Key (Hex): {private_key}\n")
+            found_file.write(f"Public Key: {public_key}\n")
+        return True
+    return False
 
 if __name__ == '__main__':
     num_processes = cpu_count()
     pool = Pool(num_processes)
+    target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"  # Целевой адрес
 
     # Start each process with a unique identifier
-    pool.starmap(generate_key_pair, [(i,) for i in range(num_processes)])
+    pool.starmap(generate_key_pair, [(i, target_address) for i in range(num_processes)])
 
     pool.close()
     pool.join()
