001c6f7c527cde67a57f96e6b7ad2a58809bc12f
baruka99 <157349448+baruka99-lab@users.noreply.github.com>
2024-04-09 21:10:10 +0500

    Update ProjectF13.py

--- F13/Lin/ProjectF13.py
+++ F13/Lin/ProjectF13.py
@@ -1,73 +1,72 @@
 print(".........................................................................................................")
 
-import hashlib
-import fastecdsa
-from fastecdsa import keys, curve
-import random
 import multiprocessing
+import hashlib
+import binascii
+import random
+from fastecdsa import keys, curve
+from multiprocessing import cpu_count, Pool
 
-def binary_to_hex(bin_string):
-    return hex(int(bin_string, 2))[2:].zfill(len(bin_string) // 4)
+def generate_private_key():
+    return hex((random.randrange((1 << 65) - 1) + (1 << 65)))[2:].upper().zfill(64)
 
-def worker(num_zeros, num_ones, stop_event):
-    target_hash = "20d45a6a762535700ce9e0b216e31994335db8a5"
+def private_key_to_public_key(private_key, compressed=True):
+    key = keys.get_public_key(int(private_key, 16), curve.secp256k1)
+    if compressed:
+        return '02' + hex(key.x)[2:].zfill(64) if key.y % 2 == 0 else '03' + hex(key.x)[2:].zfill(64)
+    else:
+        return '04' + (hex(key.x)[2:].zfill(64) + hex(key.y)[2:].zfill(64))
 
+def public_key_to_address(public_key):
+    alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    var = hashlib.new('ripemd160')
+    encoding = binascii.unhexlify(public_key.encode())
+    var.update(hashlib.sha256(encoding).digest())
+    var_encoded = ('00' + var.hexdigest()).encode()
+    digest = hashlib.sha256(binascii.unhexlify(var_encoded)).digest()
+    var_hex = '00' + var.hexdigest() + hashlib.sha256(digest).hexdigest()[0:8]
+    count = [char != '0' for char in var_hex].index(True) // 2
+    n = int(var_hex, 16)
+    output = []
+    while n > 0:
+        n, remainder = divmod(n, 58)
+        output.append(alphabet[remainder])
+    for i in range(count):
+        output.append(alphabet[0])
+    return ''.join(output[::-1])
+
+def generate_key_pair(process_id, target_address, compressed=True):
     while True:
-        if stop_event.is_set():
+        private_key = generate_private_key()
+        public_key = private_key_to_public_key(private_key, compressed=compressed)
+        address = public_key_to_address(public_key)
+
+        # Check and write address to file
+        if check_and_write_address(process_id, public_key, address, private_key, target_address):
             break
 
-        bits = ['0'] * num_zeros + ['1'] * (num_ones - 1)
-        random.shuffle(bits)
+def check_and_write_address(process_id, public_key, bitcoin_address, private_key, target_address):
+    #print(f"Process {process_id}: Private Key: {private_key}")
+    #print(f"Process {process_id}: Bitcoin Address: {bitcoin_address}\n")
 
-        bits.insert(0, '1')
-        private_key_bin = ''.join(bits)
-
-        private_key_bin = '0' * (256 - 66) + private_key_bin
-        private_key_hex = binary_to_hex(private_key_bin)
-
-        # Generate key pair
-        sk, _ = keys.gen_keypair(curve=curve.secp256k1)
-        
-        # Get public key
-        public_key = keys.get_public_key(sk, curve=curve.secp256k1)
-
-        # Convert d to an integer if it's a tuple
-        d = sk[0] if isinstance(sk, tuple) else sk
-
-        compressed_public_key = '02' + public_key[0] if public_key[1] % 2 == 0 else '03' + public_key[0]
-        compressed_public_key_bytes = bytes.fromhex(compressed_public_key)
-
-        ripemd160_hash = hashlib.new('ripemd160')
-        ripemd160_hash.update(hashlib.sha256(compressed_public_key_bytes).digest())
-        hashed_compressed_public_key = ripemd160_hash.digest().hex()
-
-        if hashed_compressed_public_key == target_hash:
-            print(private_key_hex)
-            print("hash160:", hashed_compressed_public_key)
-            print("Target hash found!")
-
-            stop_event.set()
-            break
-
-def main():
-    num_processes = multiprocessing.cpu_count()
-    processes = []
-    stop_event = multiprocessing.Event()
-
-    for _ in range(num_processes):
-        process = multiprocessing.Process(target=worker, args=(31, 35, stop_event))
-        processes.append(process)
-
-    for process in processes:
-        process.start()
-
-    for process in processes:
-        process.join()
-
-    if stop_event.is_set():
-        for process in processes:
-            process.terminate()
+    if bitcoin_address == target_address:
+        print(f"Process {process_id}: Target Address Found!")
+        print(f"Target Address: {bitcoin_address}")
+        print(f"Private Key: {private_key}")
+        with open('F13.txt', 'a') as found_file:
+            found_file.write(f"Found Target Address: {bitcoin_address}\n")
+            found_file.write(f"Private Key (Hex): {private_key}\n")
+            found_file.write(f"Public Key: {public_key}\n")
+        return True
+    return False
 
 if __name__ == '__main__':
-    main()
+    num_processes = cpu_count()
+    pool = Pool(num_processes)
+    target_address = "13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so"  # Целевой адрес
 
+    # Start each process with a unique identifier
+    pool.starmap(generate_key_pair, [(i, target_address) for i in range(num_processes)])
+
+    pool.close()
+    pool.join()
